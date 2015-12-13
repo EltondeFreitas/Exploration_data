eef7f7b5481d2b8b906f7c036aed336f3c2e4986
Elton de Freitas <EltondeFreitas@users.noreply.github.com>
2015-12-13 21:12:13 -0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+## Download the file
+download.file(url = curl,destfile = "./data/ElectricPconsumption.zip",method = "auto")
+
+## Unzip the file
+unzip("./data/ElectricPconsumption.zip", exdir = "./data")
+
+## Read the file
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## Convert the var
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+## Make the graph
+png("./data/plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
